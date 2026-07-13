b90d12cc2d44ce5bc026aa740105d39b18a3eeea
trodem <demis.troisi@stadlerrail.com>
2026-07-13 08:16:45 +0200

    fix: update server host configuration and correct application URL in docker-compose

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,8 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: true, // Ti permette di esporre il server in rete locale automaticamente
+    allowedHosts: ["star0002210001m"], // Risolve l'errore "This host is not allowed"
     port: 5173,
     proxy: {
       // Proxy API + websocket to the backend during development.
